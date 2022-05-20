b7a49bc073fc4526765b66360d1c1f486f2e6876
Andrei <severand2303@yandex.ru>
2022-05-20 10:39:51 +0300

    исправил функцию get_candidates_by_skil для корректного поиска

--- sky_lesson/Cjurse_11/utils.py
+++ sky_lesson/Cjurse_11/utils.py
@@ -1,3 +1,4 @@
+# from config import DATA_PATH
 import json
 
 with open('candidates.json', encoding='UTF-8') as f:
@@ -20,11 +21,10 @@ def get_candidate(candidate_id):
 
 def get_candidates_by_name(candidate_name):
     """Возвращает список кандидатов по совпадению в имени"""
-    list_count_name = []  # список совпадений
+    list_count_name = []
     for dict in profile:
-        if candidate_name not in dict['name']:
-            continue
-        list_count_name.append(dict['name'])
+        if dict["name"].title().count(candidate_name.title()):
+            list_count_name.append(dict['name'])
     return list_count_name
 
 
@@ -32,7 +32,6 @@ def get_candidates_by_skill(skill_name):
     """Возвращает список кандидатов по навыку"""
     list_candidates_by_skill = []
     for dict in profile:
-        if skill_name not in dict['skills']:
-            continue
-        list_candidates_by_skill.append(dict['name'])
+        if dict["skills"].title().count(skill_name.title()):
+            list_candidates_by_skill.append(dict['name'])
     return list_candidates_by_skill
